2df1494b0465a65e4572d7fa0aea8fc61d7cd0c7
jay-admin <162289105+jaypithva@users.noreply.github.com>
2026-02-19 11:25:21 +0530

    Update firebase-init.js

--- jvbet/assets/js/firebase-init.js
+++ jvbet/assets/js/firebase-init.js
@@ -1,17 +1,15 @@
+// jvbet/assets/js/firebase-init.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCAfH5_1RjVHyZLwkj26zQPCq0-8S0fSKA",
+  apiKey: "PASTE_YOUR_REAL_API_KEY_HERE",
   authDomain: "jvbet-bbf90.firebaseapp.com",
   projectId: "jvbet-bbf90",
-  storageBucket: "jvbet-bbf90.firebasestorage.app",
+  storageBucket: "jvbet-bbf90.appspot.com",
   messagingSenderId: "824871692224",
   appId: "1:824871692224:web:f74baa642b1f43b78aca25"
 };
 
-const app = initializeApp(firebaseConfig);
-
-export const auth = getAuth(app);
+export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
